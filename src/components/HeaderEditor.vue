0c5fd226b5ad4bc66d173bb363f5c925874749ce
damienjacinto <damien.jacinto@gmail.com>
2022-04-19 22:57:40 +0200

    add HeaderEditor

---
+++ src/components/HeaderEditor.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="titleEditor" v-bind:class="{ lightDarkMode: darkmodebool }">
+    <ul>
+      <li v-bind:class="{ darkMode: darkmodebool }">{{ title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header-editor",
+  props: {
+    darkmode: {
+      type: String,
+      default: "false"
+    },
+    title: {
+      type: String,
+      default: "title"
+    }
+  },
+  computed: {
+    darkmodebool: function() {
+      return (this.darkmode === 'true')
+    }
+  }
+}
+</script>
